f7337c4533bb5643ebd32b541ac358c3190a7258
Arjun Bahl <arjunbahl64@gmail.com>
2023-03-17 19:36:09 -0700

    More formatting and More AP's

--- assessment_and_plan.py
+++ assessment_and_plan.py
@@ -19,8 +19,26 @@ PRNs:''',
         "UTI": '''Symptoms:
 UA results:
 UCx/Sensi results:
-Abx: 
-'''
+Abx: ''',
+
+        "GIB": '''Baseline Hgb:
+Current Hgb:
+Shock index/Hemodynamics: 
+Actively bleeding:
+Source:
+Core Management: 2 Large bore IV's, maintain active T&S, transfuse Hgb < 7, Hold NSAIDs, antiplatelets, anticoagulants
+including DVT PPx
+Additions: IV PPI, Octreotide, Ceftriaxone
+Timing of GI consult:''',
+
+        "CHF": '''Phenotype: HFrEF
+Chronic etiology: Ischemic, HOCM, Sarcoid, Drug-related
+Acute etiology: https://www.timeofcare.com/failures-acute-heart-failure-is-precipitated-by-failures/
+Last TTE: Date, EF, LVEDD, Other significant findings
+Preload: Edema, IVC, Dry weight, Current weight, 24h I/O, Loop diuretics, Nephron blockade
+Contractility: Beta-blockers (Don't reflexively discontinue)
+Afterload/GDMT:
+Monitoring: Sodium, Potassium, Creatinine''',
     }
 
     return assessments_and_plans.get(problem, "")
